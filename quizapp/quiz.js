9c6476f45f1f792df2586f41c08a6e94753284ac
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-01-22 11:53:05 +0545

    added options

--- quizapp/quiz.js
+++ quizapp/quiz.js
@@ -1,11 +1,12 @@
- const questions=[{
+const questions = [{
     'que': 'Which of the following is a markup language?',
     'a': 'HTML',
     'b': 'CSS',
     'c': 'JavaScript',
     'd': 'PHP',
     'correct': 'a'
- }, {
+},
+{
     'que': 'when was javascript launched?',
     'a': '1999',
     'b': '1995',
@@ -20,13 +21,25 @@
     'c': 'JavaScript',
     'd': 'PHP',
     'correct': 'b'
-}]
+}];
 
 
-let index=0;
-const quesBox= document.getElementById("quesBox")
-const loadQuestion=() =>{
-    const data =questions[index]
-quesBox.innerText=   ` ${index+1} ${data.que}`  ;
+let index = 0;
+const quesBox = document.getElementById("quesBox")
+const optionInputs = document.querySelectorAll(".options")
+const loadQuestion = () => {
+    const data = questions[index]
+    quesBox.innerText = ` ${index + 1} )${data.que}`;
+    optionInputs[0].nextElementSibling.innerText = data.a;
+    optionInputs[1].nextElementSibling.innerText = data.b;
+    optionInputs[2].nextElementSibling.innerText = data.c;
+    optionInputs[3].nextElementSibling.innerText = data.d;
+
 }
+
+const submitQuiz=()=>{
+    const ans=getAnswer()
+}
+const getAnswer=()=>
+
 loadQuestion();
