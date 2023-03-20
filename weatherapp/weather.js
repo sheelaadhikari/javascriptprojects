b2e603558b836d9229dc4fc277324f4d4f7dc4d4
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-03-20 21:14:46 +0545

    weather app

--- weatherapp/weather.js
+++ weatherapp/weather.js
@@ -6,24 +6,58 @@ const options = {
 	}
 };
 
-fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Kathmandu', options)
-	.then(response => response.json())
-	.then(response => {
+const cloud_pct=document.getElementById("cloud_pct")
+const temp=document.getElementById("temp")
+const temp2=document.getElementById("temp2")
 
-		
-		console.log(response)
+const min_temp=document.getElementById("min_temp")
+const max_temp=document.getElementById("max_temp")
+const humidity=document.getElementById("humidity")
+const humidity2=document.getElementById("humidity2")
+
+const sunrise=document.getElementById("sunrise")
+const sunset=document.getElementById("sunset")
+const wind_speed=document.getElementById("wind_speed")
+const wind_speed2=document.getElementById("wind_speed2")
+
+const feels_like=document.getElementById("feels_like")
 
 
-		cloud_pct = respose.cloud_pct
-		temp = respose.temp
-		feels_like = respose.feels_like
-		humidity = respose.humidity
-		min_temp = respose.min_temp
-		max_temp = respose.max_temp
-		wind_speed = respose.wind_speed
-		wind_degrees = respose.wind_degrees
-		sunrise = respose.sunrise
-		sunset = respose.sunset
-	})
 
-	.catch(err => console.error(err));
+const getWeather = (city) => {
+	cityName.innerHTML=city;
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city='+city, options)
+		.then(response => response.json())
+		.then(response => {
+
+
+			console.log(response)
+
+
+			cloud_pct.innerHTML = response.cloud_pct
+			temp.innerHTML = response.temp
+			temp2.innerHTML = response.temp
+
+			feels_like.innerHTML = response.feels_like
+			humidity.innerHTML = response.humidity
+			humidity2.innerHTML = response.humidity
+
+			min_temp.innerHTML = response.min_temp
+			max_temp.innerHTML = response.max_temp
+			wind_speed.innerHTML = response.wind_speed
+			wind_speed2.innerHTML = response.wind_speed
+
+			sunrise.innerHTML = response.sunrise
+			sunset.innerHTML = response.sunset
+		})
+
+		.catch(err => console.error(err));
+}
+
+
+submit.addEventListener("click", (e) => {
+	e.preventDefault()
+	getWeather(city.value);
+
+})
+getWeather("Kathmandu");
