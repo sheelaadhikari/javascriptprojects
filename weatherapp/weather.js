5a44153de790dea86da803edb459b8dd66c70057
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-03-20 15:29:52 +0545

    adding values

--- weatherapp/weather.js
+++ weatherapp/weather.js
@@ -8,5 +8,22 @@ const options = {
 
 fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Kathmandu', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then(response => {
+
+		
+		console.log(response)
+
+
+		cloud_pct = respose.cloud_pct
+		temp = respose.temp
+		feels_like = respose.feels_like
+		humidity = respose.humidity
+		min_temp = respose.min_temp
+		max_temp = respose.max_temp
+		wind_speed = respose.wind_speed
+		wind_degrees = respose.wind_degrees
+		sunrise = respose.sunrise
+		sunset = respose.sunset
+	})
+
 	.catch(err => console.error(err));
