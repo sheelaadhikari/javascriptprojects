cdba7f8f14162ea2b031a183840706dc9320057f
sheelaadhikari <adhikarisheela1000@gmail.com>
2023-03-23 14:16:25 +0545

    added js file

---
+++ musicplayer/mp.js
@@ -0,0 +1,10 @@
+let play_list = document.querySelector('.play_list');
+let options = document.querySelector('.options');
+
+function open_p() {
+    play_list.classList.toggle('active');
+}
+
+function sidebar() {
+    options.classList.toggle('active2');
+}
